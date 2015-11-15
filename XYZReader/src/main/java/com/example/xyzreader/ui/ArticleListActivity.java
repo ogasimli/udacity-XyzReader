b49bf5e2c49b25d5dca2076ab81325113e2c037c
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-11-15 18:07:41 +0400

    fix: Fix articles list item

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -18,10 +18,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.text.format.DateUtils;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -33,7 +29,7 @@ import butterknife.ButterKnife;
  * activity presents a grid of items as cards.
  */
 public class ArticleListActivity extends AppCompatActivity implements
-        LoaderManager.LoaderCallbacks<Cursor> {
+        LoaderManager.LoaderCallbacks<Cursor>, ArticlesAdapter.OnArticleItemClickListener {
 
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.swipe_refresh_layout) SwipeRefreshLayout mSwipeRefreshLayout;
@@ -102,13 +98,14 @@ public class ArticleListActivity extends AppCompatActivity implements
 
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        Adapter adapter = new Adapter(cursor);
-        adapter.setHasStableIds(true);
+        ArticlesAdapter adapter = new ArticlesAdapter(this, cursor);
+        adapter.setListener(this);
         mRecyclerView.setAdapter(adapter);
+
         int columnCount = getResources().getInteger(R.integer.list_column_count);
-        StaggeredGridLayoutManager sglm =
+        StaggeredGridLayoutManager staggeredGridLayoutManager =
                 new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(sglm);
+        mRecyclerView.setLayoutManager(staggeredGridLayoutManager);
     }
 
     @Override
@@ -116,66 +113,8 @@ public class ArticleListActivity extends AppCompatActivity implements
         mRecyclerView.setAdapter(null);
     }
 
-    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
-        private Cursor mCursor;
-
-        public Adapter(Cursor cursor) {
-            mCursor = cursor;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            mCursor.moveToPosition(position);
-            return mCursor.getLong(ArticleLoader.Query._ID);
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.list_item_article, parent, false);
-            final ViewHolder vh = new ViewHolder(view);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
-                }
-            });
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            mCursor.moveToPosition(position);
-            holder.titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-            holder.subtitleView.setText(
-                    DateUtils.getRelativeTimeSpanString(
-                            mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
-                            System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
-                            DateUtils.FORMAT_ABBREV_ALL).toString()
-                            + " by "
-                            + mCursor.getString(ArticleLoader.Query.AUTHOR));
-            holder.thumbnailView.setImageUrl(
-                    mCursor.getString(ArticleLoader.Query.THUMB_URL),
-                    ImageLoaderHelper.getInstance(ArticleListActivity.this).getImageLoader());
-            holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
-        }
-
-        @Override
-        public int getItemCount() {
-            return mCursor.getCount();
-        }
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public DynamicHeightNetworkImageView thumbnailView;
-        public TextView titleView;
-        public TextView subtitleView;
-
-        public ViewHolder(View view) {
-            super(view);
-            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
-            titleView = (TextView) view.findViewById(R.id.article_title);
-            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
-        }
+    @Override
+    public void onArticleSelected(long articleId) {
+        startActivity(new Intent(Intent.ACTION_VIEW, ItemsContract.Items.buildItemUri(articleId)));
     }
 }
---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticlesAdapter.java
@@ -0,0 +1,156 @@
+package com.example.xyzreader.ui;
+
+import com.bumptech.glide.Glide;
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+import com.github.florent37.glidepalette.BitmapPalette;
+import com.github.florent37.glidepalette.GlidePalette;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.BindColor;
+import butterknife.ButterKnife;
+
+/**
+ * RecyclerView adapter for articles list
+ * Created by ogasimli on 15.11.2015.
+ */
+public final class ArticlesAdapter extends RecyclerView.Adapter<ArticlesAdapter.ArticleHolder> {
+
+    public interface OnArticleItemClickListener {
+        void onArticleSelected(long articleId);
+
+        OnArticleItemClickListener DUMMY = new OnArticleItemClickListener() {
+            @Override
+            public void onArticleSelected(long articleId) {
+            }
+        };
+    }
+
+    private final Activity mActivity;
+    private final LayoutInflater mInflater;
+
+    private OnArticleItemClickListener mListener = OnArticleItemClickListener.DUMMY;
+    private Cursor mCursor;
+
+    public ArticlesAdapter(Activity activity, Cursor cursor) {
+        mInflater = LayoutInflater.from(activity);
+        mActivity = activity;
+        mCursor = cursor;
+        setHasStableIds(true);
+    }
+
+    public ArticlesAdapter setListener(@NonNull OnArticleItemClickListener listener) {
+        mListener = listener;
+        return this;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        mCursor.moveToPosition(position);
+        return mCursor.getLong(ArticleLoader.Query._ID);
+    }
+
+    @Override
+    public ArticleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ArticleHolder(mInflater.inflate(R.layout.list_item_article, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ArticleHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    final class ArticleHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.article_item_footer_container)
+        View footerContainerView;
+        @Bind(R.id.thumbnail)
+        ImageView thumbnailView;
+        @Bind(R.id.article_title)
+        TextView titleView;
+        @Bind(R.id.article_subtitle)
+        TextView subtitleView;
+
+        @BindColor(R.color.colorPrimary)
+        int mColorBackground;
+        @BindColor(R.color.color_title_inverse)
+        int mColorTitle;
+        @BindColor(R.color.color_subtitle_inverse)
+        int mColorSubtitle;
+
+        private long mLastItemId;
+
+        public ArticleHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mListener.onArticleSelected(getItemId());
+                }
+            });
+        }
+
+        public void bind(int position) {
+            mCursor.moveToPosition(position);
+
+            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+            subtitleView.setText(
+                    mActivity.getString(R.string.article_item_subtitle,
+                            DateUtils.getRelativeTimeSpanString(
+                                    mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                                    System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                                    DateUtils.FORMAT_ABBREV_ALL).toString(),
+                            mCursor.getString(ArticleLoader.Query.AUTHOR)));
+
+            if (mLastItemId != getItemId()) {
+                resetColors();
+                mLastItemId = getItemId();
+            }
+
+            String imageUrl = mCursor.getString(ArticleLoader.Query.THUMB_URL);
+            Glide.with(mActivity)
+                    .load(imageUrl)
+                    .crossFade()
+                    .placeholder(R.color.photo_placeholder)
+                    .listener(GlidePalette.with(imageUrl).intoCallBack(new BitmapPalette.CallBack() {
+                        @Override
+                        public void onPaletteLoaded(Palette palette) {
+                            applyColors(palette.getVibrantSwatch());
+                        }
+                    }))
+                    .into(thumbnailView);
+        }
+
+        private void resetColors() {
+            footerContainerView.setBackgroundColor(mColorBackground);
+            titleView.setTextColor(mColorTitle);
+            subtitleView.setTextColor(mColorSubtitle);
+        }
+
+        private void applyColors(Palette.Swatch swatch) {
+            if (swatch != null) {
+                footerContainerView.setBackgroundColor(swatch.getRgb());
+                titleView.setTextColor(swatch.getBodyTextColor());
+                subtitleView.setTextColor(swatch.getTitleTextColor());
+            }
+        }
+    }
+}

a2b03b8d74d47165940258b920e4f85825d89872
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-11-17 00:38:58 +0400

    fix: Improve fetchPlainText method and change URL

--- XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -1,17 +1,16 @@
 package com.example.xyzreader.remote;
 
-import android.util.Log;
-
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONTokener;
 
-import java.io.ByteArrayOutputStream;
+import android.util.Log;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class RemoteEndpointUtil {
@@ -21,7 +20,7 @@ public class RemoteEndpointUtil {
     }
 
     public static JSONArray fetchJsonArray() {
-        String itemsJson = null;
+        String itemsJson;
         try {
             itemsJson = fetchPlainText(Config.BASE_URL);
         } catch (IOException e) {
@@ -45,28 +44,16 @@ public class RemoteEndpointUtil {
     }
 
     static String fetchPlainText(URL url) throws IOException {
-        return new String(fetch(url), "UTF-8" );
-    }
+        OkHttpClient client = new OkHttpClient();
 
-    static byte[] fetch(URL url) throws IOException {
-        InputStream in = null;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
-        try {
-            OkHttpClient client = new OkHttpClient();
-            HttpURLConnection conn = client.open(url);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            in = conn.getInputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, bytesRead);
-            }
-            return out.toByteArray();
-
-        } finally {
-            if (in != null) {
-                in.close();
-            }
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Unexpected code " + response);
         }
+        return response.body().string();
     }
 }

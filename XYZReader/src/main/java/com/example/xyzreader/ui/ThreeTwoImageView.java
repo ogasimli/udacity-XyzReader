1456a980c35e9e98632bb30ddd05e011adae5b02
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-11-17 21:11:49 +0400

    fix: Fix dynamic image height class

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ThreeTwoImageView.java
@@ -0,0 +1,27 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ThreeTwoImageView extends ImageView {
+
+    public ThreeTwoImageView(Context context) {
+        super(context);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) *3/2;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
